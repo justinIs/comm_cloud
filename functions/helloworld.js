4106945d4b22f8788922ed12d154b78f3bc21987
Justin Williams <justin@agilityfeat.com>
2024-06-12 12:38:08 -0700

    AI

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,23 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export const Env = {
+    // If you set another name in wrangler.toml as the value for 'binding',
+    // replace "AI" with the variable name you defined.
+    AI: Ai
+}
+
+export async function onRequest(context) {
+    const res = await fetch(
+        "https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav"
+    );
+    const blob = await res.arrayBuffer();
+
+    const input = {
+        audio: [...new Uint8Array(blob)],
+    };
+
+    const response = await env.AI.run(
+        "@cf/openai/whisper",
+        input
+    );
+
+    return Response.json({input: {audio: []}, response});
 }
