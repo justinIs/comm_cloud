18ef040e521594566f67ce77ecce611a5ed33082
Justin Williams <justin@agilityfeat.com>
2024-06-12 12:41:32 -0700

    fix AI?

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,8 +1,8 @@
-export const Env = {
-    // If you set another name in wrangler.toml as the value for 'binding',
-    // replace "AI" with the variable name you defined.
-    AI: AI
-}
+// export const Env = {
+//     // If you set another name in wrangler.toml as the value for 'binding',
+//     // replace "AI" with the variable name you defined.
+//     AI: AI
+// }
 
 export async function onRequest(context) {
     const res = await fetch(
